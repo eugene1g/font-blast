9cfb71cf3e081597aa4786f1131d4705219e7990
EugeneG <eugene7@gmail.com>
2017-03-22 19:32:30 -0400

    ES6

---
+++ src/cli-font-blast.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import fs from "graceful-fs";
+import pckg from "../package.json";
+import program from "commander";
+import blaster from "../lib/font-blast/index";
+import type { UserConf } from "../lib/font-blast/index";
+
+program
+  .version(pckg.version)
+  .usage("[options] svg-font.svg outputDir")
+  .option(
+    "-i, --icons <icon-refs>",
+    "Limit the output to the selected icons. Icons can be provided with their unicode value or the full reference",
+    function(val) {
+      return val ? val.split(",") : [];
+    }
+  )
+  .option(
+    "-p, --png <heightInPx>",
+    "Include this to generate PNG files. Please note you will need to have an executable binary in your path for 'batik-rasterizer'",
+    parseInt
+  )
+  .option(
+    "-c, --color <colorcode>",
+    "Set the color of icons in the output (relevant mainly for PNG files)"
+  )
+  .parse(process.argv);
+
+const svgFontFile = program.args[0], outputDir = program.args[1];
+if (!svgFontFile || !outputDir) {
+  program.help();
+}
+
+if (!fs.existsSync(svgFontFile)) {
+  console.error("This provided SVG font file does not exist");
+  process.exit();
+}
+
+const config: UserConf = {
+  icons: program.icons,
+  png: program.png,
+  color: program.color
+};
+blaster(svgFontFile, outputDir, config);
---
+++ src/index.js
@@ -0,0 +1,75 @@
+// @flow
+import path from "path";
+import fs from "graceful-fs";
+import mkdir from "mkdirp";
+import verify from "./verify";
+import extractor from "./glyph-extractor";
+import pngGenerator from "./png-generator";
+import type { IconInformation } from "./glyph-extractor";
+
+type UserConf = {
+  png?: Boolean,
+  filenames?: Object,
+  icons?: Array<string>,
+  cleanCharacter?: Function
+};
+
+const defaultConfig: UserConf = {};
+
+function mainBlaster(
+  fontFile: string,
+  destinationFolder: string,
+  userConfig: UserConf
+) {
+  const config = Object.assign({}, defaultConfig, userConfig || {});
+  const svgFontContent = fs.readFileSync(fontFile, "utf-8");
+
+  extractor(
+    svgFontContent,
+    config.filenames,
+    function(characterSvgs: Array<IconInformation>) {
+      console.info(
+        "Found " + characterSvgs.length + " available icons in the font"
+      );
+      console.info("Generating SVG content for each character...");
+
+      const svgDir = path.resolve(
+        process.cwd(),
+        path.join(destinationFolder, "svg")
+      );
+      const pngDir = path.resolve(
+        process.cwd(),
+        path.join(destinationFolder, "png")
+      );
+
+      if (!fs.existsSync(svgDir)) mkdir.sync(svgDir);
+
+      let savedIcons = [];
+      characterSvgs.forEach((char: IconInformation) => {
+        const filename = char.name ? char.name : char.code;
+        //If a subset of icons set was requested, ignore any others that are not within the subset
+        if (
+          config.icons &&
+          config.icons.length &&
+          config.icons.indexOf(char.code) === -1 &&
+          config.icons.indexOf(filename) === -1
+        ) {
+          return;
+        }
+        savedIcons.push(char);
+        fs.writeFileSync(path.join(svgDir, filename + ".svg"), char.svg);
+      });
+      console.info("Saved " + savedIcons.length + " files to " + svgDir);
+
+      if (config.png && !isNaN(config.png)) {
+        console.info("Generating PNG images - this may take a minute...");
+        pngGenerator(svgDir, pngDir, config.png);
+      }
+
+      verify(svgFontContent, savedIcons, config.png, destinationFolder);
+    },
+    config.cleanCharacter
+  );
+}
+
+export default mainBlaster;
