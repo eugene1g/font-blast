04825833e5108f97b62fbcb7b8b7473c9ffe4407
EugeneG <eugene7@gmail.com>
2017-03-22 19:27:52 -0400

    ES6

--- bin/font-blast.js
+++ bin/font-blast.js
@@ -1,35 +1,44 @@
 #!/usr/bin/env node
-"use strict";
+import fs from "graceful-fs";
+import pckg from "../package.json";
+import program from "commander";
+import blaster from "../lib/font-blast/index";
+import type { UserConf } from "../lib/font-blast/index";
 
-var fs      = require('graceful-fs'),
-    pckg    = require('../package.json'),
-    program = require('commander');
+program
+  .version(pckg.version)
+  .usage("[options] svg-font.svg outputDir")
+  .option(
+    "-i, --icons <icon-refs>",
+    "Limit the output to the selected icons. Icons can be provided with their unicode value or the full reference",
+    function(val) {
+      return val ? val.split(",") : [];
+    }
+  )
+  .option(
+    "-p, --png <heightInPx>",
+    "Include this to generate PNG files. Please note you will need to have an executable binary in your path for 'batik-rasterizer'",
+    parseInt
+  )
+  .option(
+    "-c, --color <colorcode>",
+    "Set the color of icons in the output (relevant mainly for PNG files)"
+  )
+  .parse(process.argv);
 
-program.
-    version(pckg.version).
-    usage("[options] svg-font.svg outputDir").
-    option('-i, --icons <icon-refs>', "Limit the output to the selected icons. Icons can be provided with their unicode value or the full reference", function (val) {
-        return val ? val.split(',') : [];
-    }).
-    option('-p, --png <heightInPx>', "Include this to generate PNG files. Please note you will need to have an executable binary in your path for 'batik-rasterizer'", parseInt).
-    option('-c, --color <colorcode>', "Set the color of icons in the output (relevant mainly for PNG files)").
-    parse(process.argv)
-;
-
-var svgFontFile = program.args[0], outputDir = program.args[1];
+const svgFontFile = program.args[0], outputDir = program.args[1];
 if (!svgFontFile || !outputDir) {
-    program.help();
+  program.help();
 }
 
 if (!fs.existsSync(svgFontFile)) {
-    console.error("This provided SVG font file does not exist");
-    return;
+  console.error("This provided SVG font file does not exist");
+  process.exit();
 }
 
-
-var config = {
-    icons: program.icons,
-    png:   program.png,
-    color: program.color
+const config: UserConf = {
+  icons: program.icons,
+  png: program.png,
+  color: program.color
 };
-require('../lib/font-blast/index')(svgFontFile, outputDir, config);
+blaster(svgFontFile, outputDir, config);
